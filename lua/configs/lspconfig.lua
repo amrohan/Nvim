baacb9c9eeb52b33f69c889429ac2534a6108423
amrohan <rohanxsalunkhe@gmail.com>
2024-05-14 20:18:14 +0530

    add:angularls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "angularls", "csharp_ls", "gopls", "tsserver", "rust_analyzer" }
+local servers = { "html", "cssls", "csharp_ls", "gopls", "tsserver", "rust_analyzer" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,9 +15,31 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+local ok, mason_registry = pcall(require, "mason-registry")
+if not ok then
+  vim.notify "mason-registry could not be loaded"
+  return
+end
+
+local angularls_path = mason_registry.get_package("angular-language-server"):get_install_path()
+
+local cmd = {
+  "ngserver",
+  "--stdio",
+  "--tsProbeLocations",
+  table.concat({
+    angularls_path,
+    vim.uv.cwd(),
+  }, ","),
+  "--ngProbeLocations",
+  table.concat({
+    angularls_path .. "/node_modules/@angular/language-server",
+    vim.uv.cwd(),
+  }, ","),
+}
+lspconfig.angularls.setup {
+  cmd = cmd,
+  on_new_config = function(new_config, new_root_dir)
+    new_config.cmd = cmd
+  end,
 }
