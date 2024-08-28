5856701bfbcd2c9ba5f4bec534689c60b5eec6f0
amrohan <rohanxsalunkhe@gmail.com>
2024-08-28 20:33:34 +0530

    add:fold depenging upon file type

--- lua/options.lua
+++ lua/options.lua
@@ -5,5 +5,19 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 local o = vim.o
-o.foldmethod = "indent"
-o.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldenable = false
+o.foldnestmax = 10
+o.foldlevel = 2
+-- o.foldmethod = "expr"
+-- o.foldexpr = "nvim_treesitter#foldexpr()"
+--
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  callback = function()
+    if require("nvim-treesitter.parsers").has_parser() then
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    else
+      vim.opt.foldmethod = "syntax"
+    end
+  end,
+})
