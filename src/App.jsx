a8c7288793e0f879f176397ad89433cef411f556
Rohan <rohan@Rohans-MacBook-Air.local>
2024-11-24 13:04:37 +0530

    setup tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className=''>
+        <h1>Todo App</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
+
+
+
+function Todo() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
